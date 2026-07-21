a11595c3919715abc31edbdc7a6c23224d83d457
BouncingElf10 <168445249+BouncingElf10@users.noreply.github.com>
2026-07-21 20:44:41 +0200

    feat(block): added specific spawn dispenser behavior

--- pumpkin/src/block/blocks/redstone/dispenser.rs
+++ pumpkin/src/block/blocks/redstone/dispenser.rs
@@ -1,6 +1,7 @@
 use rand::{Rng, RngExt, rng};
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use uuid::Uuid;
 
 use crate::block::blocks::redstone::block_receives_redstone_power;
 use crate::block::registry::BlockActionResult;
@@ -8,15 +9,23 @@ use crate::block::{
     BlockBehaviour, BlockFuture, GetComparatorOutputArgs, NormalUseArgs, OnNeighborUpdateArgs,
     OnPlaceArgs, OnScheduledTickArgs, PlacedArgs,
 };
+use crate::entity::decoration::armor_stand::ArmorStandEntity;
 use crate::entity::item::ItemEntity;
 use crate::entity::projectile::arrow::{ArrowEntity, ArrowPickup};
+use crate::entity::tnt::TNTEntity;
+use crate::entity::r#type::from_type;
+use crate::entity::vehicle::boat::BoatEntity;
 use crate::entity::{Entity, EntityBase};
+use crate::item::ItemMetadata;
+use crate::item::items::boat::BoatItem;
+use crate::item::items::spawn_egg::apply_entity_variant;
 use crate::world::World;
 
 use crate::block::entities::dispenser::DispenserBlockEntity;
 use pumpkin_data::BlockStateId;
 use pumpkin_data::block_properties::{BlockProperties, Facing};
-use pumpkin_data::entity::EntityType;
+use pumpkin_data::entity::{EntityType, entity_from_egg};
+use pumpkin_data::fluid::Fluid;
 use pumpkin_data::item::Item;
 use pumpkin_data::item_stack::ItemStack;
 use pumpkin_data::sound::{Sound, SoundCategory};
@@ -30,8 +39,10 @@ use pumpkin_inventory::screen_handler::{
 use pumpkin_macros::pumpkin_block;
 use pumpkin_protocol::IdOr;
 use pumpkin_protocol::java::client::play::CSoundEffect;
+use pumpkin_util::math::boundingbox::{BoundingBox, EntityDimensions};
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_util::math::vector3::Vector3;
+use pumpkin_util::math::wrap_degrees;
 use pumpkin_util::text::TextComponent;
 use pumpkin_world::inventory::Inventory;
 use pumpkin_world::tick::TickPriority;
@@ -191,10 +202,27 @@ impl BlockBehaviour for DispenserBlock {
 
                     // Still missing some specific dispenser behavior that you can find here:
                     // https://minecraft.wiki/w/Dispenser#Usage
-                    let arrows = [Item::ARROW.id, Item::TIPPED_ARROW.id, Item::SPECTRAL_ARROW.id];
+                    let arrows = [
+                        Item::ARROW.id,
+                        Item::TIPPED_ARROW.id,
+                        Item::SPECTRAL_ARROW.id,
+                    ];
+                    let boats = BoatItem::ids();
 
                     if arrows.contains(&item.item.id) {
                         Self::fire_arrow(&ctx, &mut item).await;
+                    } else if boats.contains(&item.item.id) {
+                        if !Self::dispense_boat(&ctx, &mut item).await {
+                            Self::drop_item(&ctx, &mut item).await;
+                        }
+                    } else if item.item.id == Item::ARMOR_STAND.id {
+                        if !Self::dispense_armor_stand(&ctx, &mut item).await {
+                            Self::drop_item(&ctx, &mut item).await;
+                        }
+                    } else if item.item.id == Item::TNT.id {
+                        Self::dispense_tnt(&ctx, &mut item).await;
+                    } else if entity_from_egg(item.item.id).is_some() {
+                        Self::dispense_spawn_egg(&ctx, &mut item).await;
                     } else {
                         Self::drop_item(&ctx, &mut item).await;
                     }
@@ -275,6 +303,122 @@ impl DispenserBlock {
             *ctx.position,
             to_data3d(ctx.facing),
         );
+    }
+
+    fn target_position(ctx: &DispenseContext<'_>) -> BlockPos {
+        let facing = to_normal(ctx.facing);
+        ctx.position.offset(Vector3::new(
+            facing.x as i32,
+            facing.y as i32,
+            facing.z as i32,
+        ))
+    }
+
+    fn has_room_for(
+        ctx: &DispenseContext<'_>,
+        spawn_pos: Vector3<f64>,
+        size: &EntityDimensions,
+    ) -> bool {
+        let bounding_box = BoundingBox::new_from_pos(spawn_pos.x, spawn_pos.y, spawn_pos.z, size);
+        ctx.world.is_space_empty(bounding_box)
+            && ctx.world.get_entities_at_box(&bounding_box).is_empty()
+    }
+
+    async fn dispense_boat(ctx: &DispenseContext<'_>, item: &mut ItemStack) -> bool {
+        let target = Self::target_position(ctx);
+        if ctx.world.get_fluid(&target).id != Fluid::WATER.id {
+            return false;
+        }
+
+        let entity_type = BoatItem::item_to_entity(item.item);
+        let dimensions = EntityDimensions::new(
+            entity_type.dimension[0],
+            entity_type.dimension[1],
+            entity_type.eye_height,
+        );
+        let spawn_pos = target.to_f64();
+        if !Self::has_room_for(ctx, spawn_pos, &dimensions) {
+            return false;
+        }
+
+        let _ = item.split(1);
+        let facing = to_normal(ctx.facing);
+        let entity = Entity::new(ctx.world.clone(), spawn_pos, entity_type);
+        entity.set_rotation(facing.x.atan2(facing.z) as f32 * 57.295_776, 0.0);
+        ctx.world
+            .spawn_entity(Arc::new(BoatEntity::new(entity)))
+            .await;
+
+        ctx.world
+            .sync_world_event(WorldEvent::SoundDispenserDispense, *ctx.position, 0);
+        true
+    }
+
+    async fn dispense_armor_stand(ctx: &DispenseContext<'_>, item: &mut ItemStack) -> bool {
+        let target = Self::target_position(ctx);
+        let spawn_pos = target.to_f64();
+        let dimensions = EntityDimensions::new(
+            EntityType::ARMOR_STAND.dimension[0],
+            EntityType::ARMOR_STAND.dimension[1],
+            EntityType::ARMOR_STAND.eye_height,
+        );
+        if !Self::has_room_for(ctx, spawn_pos, &dimensions) {
+            return false;
+        }
+
+        let _ = item.split(1);
+        let facing = to_normal(ctx.facing);
+        let entity = Entity::new(ctx.world.clone(), spawn_pos, &EntityType::ARMOR_STAND);
+        entity.set_rotation(facing.x.atan2(facing.z) as f32 * 57.295_776, 0.0);
+
+        ctx.world.play_sound(
+            Sound::EntityArmorStandPlace,
+            SoundCategory::Blocks,
+            &spawn_pos,
+        );
+        ctx.world
+            .spawn_entity(Arc::new(ArmorStandEntity::new(entity)))
+            .await;
+
+        ctx.world
+            .sync_world_event(WorldEvent::SoundDispenserDispense, *ctx.position, 0);
+        true
+    }
+
+    async fn dispense_tnt(ctx: &DispenseContext<'_>, item: &mut ItemStack) {
+        const TNT_POWER: f32 = 4.0;
+        const TNT_FUSE: u32 = 80;
+
+        let _ = item.split(1);
+        let spawn_pos = Self::target_position(ctx).to_f64();
+
+        let entity = Entity::new(ctx.world.clone(), spawn_pos, &EntityType::TNT);
+        let tnt = Arc::new(TNTEntity::new(entity, TNT_POWER, TNT_FUSE));
+        ctx.world.spawn_entity(tnt).await;
+        ctx.world
+            .play_sound(Sound::EntityTntPrimed, SoundCategory::Blocks, &spawn_pos);
+
+        ctx.world
+            .sync_world_event(WorldEvent::SoundDispenserDispense, *ctx.position, 0);
+    }
+
+    async fn dispense_spawn_egg(ctx: &DispenseContext<'_>, item: &mut ItemStack) {
+        let Some(entity_type) = entity_from_egg(item.item.id) else {
+            return;
+        };
+
+        let _ = item.split(1);
+        let spawn_pos = Self::target_position(ctx).to_f64();
+
+        let mob = from_type(entity_type, spawn_pos, ctx.world, Uuid::new_v4());
+        let yaw = wrap_degrees(rand::random::<f32>() * 360.0) % 360.0;
+        mob.get_entity().set_rotation(yaw, 0.0);
+        apply_entity_variant(item, mob.as_ref());
+
+        ctx.world.spawn_entity(mob).await;
+
+        ctx.world
+            .sync_world_event(WorldEvent::SoundDispenserDispense, *ctx.position, 0);
     }
 
     async fn drop_item(ctx: &DispenseContext<'_>, item: &mut ItemStack) {
